d8a69935e2debeba0a58e4c2da623df581f14c9f
Jameson-Ryan <jamrya0726@students.maquoketaschools.org>
2025-10-27 14:32:07 -0500

    Update index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+let firstName = 'James';
+let jobTitle = 'Programmer';
+let city = 'Maquoketa';
+
+let newJobPost = `Hello, my name is ${firstName}, and I just got a new job in ${city} as a ${jobTitle}!`;
+
+document.getElementById("result").innerHTML = newJobPost;
